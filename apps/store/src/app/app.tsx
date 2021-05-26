14ff7a6efcba186e0c60afe7937aa13410375089
Koushik Sadhukhan <kousadhu@publicisgroupe.net>
2021-05-26 21:25:34 +0530

    createing shared lib and created one header component

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -6,39 +6,45 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
+import { Header } from '@nx-fullstack-app/store/ui-shared';
+import './app.scss';
+
 export const App = () => {
   return (
-    <div className="container">
-      <div className="games-layout">
-        {getAllGames().map((x) => (
-          <Card key={x.id} className="game-card">
-            <CardActionArea>
-              <CardMedia
-                className="game-card-media"
-                image={x.image}
-                title={x.name}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {x.name}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                  {x.description}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
-                  className="game-rating"
-                >
-                  <strong>Rating:</strong> {x.rating}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
+    <>
+      <Header />
+      <div className="container">
+        <div className="games-layout">
+          {getAllGames().map((x) => (
+            <Card key={x.id} className="game-card">
+              <CardActionArea>
+                <CardMedia
+                  className="game-card-media"
+                  image={x.image}
+                  title={x.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {x.name}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {x.description}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    className="game-rating"
+                  >
+                    <strong>Rating:</strong> {x.rating}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
